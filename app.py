822d60833cc32f4d6c41753eded26f1e8fada558
xxxgadget <118954429+xxxgadget@users.noreply.github.com>
2023-10-09 13:05:15 +0000

    add asset folder

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, app
+from flask.templating import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello World!</p>"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
